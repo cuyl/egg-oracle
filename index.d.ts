b78cc979638a7552ad29d8cbf979d99562985e5c
cuyl <l.yuch@foxmail.com>
2018-04-09 11:09:01 +0800

    deps: update oracledb 2.2.0 add @types/oracledb add index.d.ts

---
+++ index.d.ts
@@ -0,0 +1,54 @@
+/// <reference types="oracledb" />
+/// <reference types="egg" />
+import { IExecuteOptions, IExecuteReturn, IPoolAttributes, IConnection, IPromise } from 'oracledb';
+
+interface OracleDB {
+  config: any;
+  getConnection(): Promise<OracleConnection>;
+  execute(sql: string, bindParams: Object|Array<any>, options: IExecuteOptions): Promise<IExecuteReturn>;
+  close(): Promise<void>;
+  destroy(): Promise<void>;
+}
+
+interface OracleConnection {
+  /**
+   * orabledb Connection
+   */
+  _connection: IConnection;
+  execute(sql: string, bindParams: Object|Array<any>, options: IExecuteOptions): Promise<IExecuteReturn>;
+  rollback(): IPromise<void>;
+  commit(): IPromise<void>;
+  close(): IPromise<void>;
+}
+
+interface ClinetConfig {
+  user: string;
+  password: string;
+  connectString: string;
+}
+
+interface SingleOracleConfig {
+  client: ClinetConfig;
+  app: Boolean;
+  agent: Boolean;
+}
+
+interface MultiOracleConfig {
+  clients: {
+    [key:string]: ClinetConfig;
+  };
+  app: Boolean;
+  agent: Boolean;
+}
+
+declare module 'egg' {
+  interface Application {
+    oracle: Singleton<OracleDB> | OracleDB;
+  }
+  interface Singleton<T> {
+    get(id: string): T;
+  }
+  interface EggAppConfig {
+    oracle: SingleOracleConfig | MultiOracleConfig;
+  }
+}
